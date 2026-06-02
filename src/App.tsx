a28d5709362cf3260bc6ea5a50149c2388bfdeb5
Ahmad-mustapha <ahmadishola12@gmail.com>
2026-06-02 20:34:24 +0100

    fixes

--- src/App.tsx
+++ src/App.tsx
@@ -1,18 +1,18 @@
-import { HashRouter, Routes, Route } from 'react-router-dom'
+import { BrowserRouter, Routes, Route } from 'react-router-dom'
 import Home from './pages/Home'
 import Access from './pages/Access'
 import Layout from './Layout'
 
 const App = () => {
   return (
-    <HashRouter>
+    <BrowserRouter>
       <Routes>
         <Route path="/" element={<Layout />}>
           <Route index element={<Home />} />
           <Route path="access" element={<Access />} />
         </Route>
       </Routes>
-    </HashRouter>
+    </BrowserRouter>
   )
 }
 
